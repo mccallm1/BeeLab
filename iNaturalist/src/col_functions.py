41ad341e2e8c29fa48c4c498dee17847c62d42a9
Miles McCall <mccallm@oregonstate.edu>
2019-04-19 20:05:07 -0700

    Reworked output generating function and associated functions. (Large update)

--- iNaturalist/src/col_functions.py
+++ iNaturalist/src/col_functions.py
@@ -1,3 +1,7 @@
+import os
+import sys
+import elevation
+
 def test_import():
     print("col_functions.py test")
 
@@ -10,6 +14,141 @@ def test_import():
 # the interpretation of input data as modular as possible. Each eval function
 # uses potentially different logic to generate properly formatted output values,
 # and each function can be modified independently.
+
+def collector_name(in_file, user_name):
+    # Open usernames CSV
+    with open(in_file,'r') as file:
+        for row in file:
+            # Remove trailing characters and split line into array
+            row = row.rstrip("\r\n")
+            row = row.split(',')
+            # Found a match...
+            if row[1] == user_name:
+                # First name: 1st word of column 1
+                first_name = row[0].split(' ')[0]
+                # First letter of the first name
+                first_initial = first_name[0] + '.'
+                # Last name: 2nd word of column 1
+                last_name = row[0].split(' ')[1]
+                #Done
+                return first_name, first_initial, last_name
+    return "","",""
+
+def date_1(in_date):
+    # Check input
+    if in_date == '':
+        return '','',''
+    # Init vars
+    in_date = in_date.split('/')
+    month_numeral = ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XIII']
+    # Parse values from full date string
+    day = in_date[1]
+    # Reference numeral array
+    month = month_numeral[int(in_date[0]) - 1]
+    year = in_date[2]
+    return day, month, year
+
+def time_1(in_time):
+    # Check input
+    if in_time == '':
+        return ''
+    # Split full time string to remove date (1st word)
+    in_time = in_time.split(' ')
+    # Split time word by : to separate hours, mins, secs
+    return_time = in_time[1].split(':')
+    # Reattach the hours and minutes, leaving out seconds
+    return_time = return_time[0] + ":" + return_time[1]
+    return return_time
+
+def date_2(in_date):
+    # Check input
+    if in_date == '':
+        return '','','',''
+    # Init vars
+    month_numeral = ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XIII']
+
+    in_date = in_date.split('T')
+    # There are several input formats for this col
+    # If the format can be split with T continue:
+    if len(in_date) == 2:
+        #print("Splitting with T...")
+        in_date = in_date[0].split('-')
+        # Parse values from full date string
+        day = in_date[2]
+        # Reference numeral array
+        month = month_numeral[int(in_date[1]) - 1]
+        # Year is straight forward
+        year = in_date[0]
+        # Calculate merge string
+        merge = "-" + day + month
+    else:
+        #print("Splitting without T...")
+        in_date = in_date[0].split(' ')
+        in_date = in_date[0].split('/')
+        # Parse values from full date string
+        day = in_date[0]
+        # Reference numeral array
+        month = month_numeral[int(in_date[1]) - 1]
+        # Year is straight forward
+        year = in_date[2]
+        # Calculate merge string
+        merge = "-" + day + month
+
+    return day, month, year, merge
+
+def time_2(in_time):
+    # Check input
+    if in_time == '':
+        return ''
+
+    # Split full time string to remove date (1st word)
+    in_time = in_time.split('T')
+
+    if len(in_time) == 2:
+        #print("Splitting with T...")
+        in_time = in_time[1].split(':')
+        in_time = in_time[0] + ":" + in_time[1]
+    else:
+        #print("Splitting without T...")
+        in_time = in_time[0].split(' ')
+        in_time = in_time[1]
+
+    return in_time
+
+def location_guess(address, cities_file):
+    #print("raw:",address)
+
+    address = address.split(", ")
+    #print("split on comma:",address)
+
+    # If 'normal' address, should split in 4
+    if len(address) == 4:
+        #print("guess:",address[1])
+        return address[1]
+    else:
+        #print("unusual format...")
+        return ""
+
+def round_coord(coord):
+    temp = '%.4f'%(float(coord))
+    if len(temp.split('.')[1]) < 4:
+        print("coordinate didn't have 4 digits:",temp)
+        temp = float(str(temp) + "0")
+        print("fixed(?):",temp)
+        sys.exit()
+
+    return temp
+
+def elevation_from_coords(lat,long):
+    print("Calculating elevation...")
+    # clip the SRTM1 30m DEM of Rome and save it to Rome-DEM.tif
+    elevation.clip(bounds=(12.35, 41.8, 12.65, 42), output='./Rome-DEM.tif')
+    # clean up stale temporary files and fix the cache in the event of a server error
+    #elevation.clean()
+    print("saved test in file")
+
+
+##################################
 
 def eval_iNaturalistID(result_array, id_string):
     result_array.append(str(id_string))
--- iNaturalist/src/format_data.py
+++ iNaturalist/src/format_data.py
@@ -11,6 +11,7 @@ import os
 import sys
 import string
 import errno
+import csv
 import openpyxl
 from openpyxl import load_workbook
 from openpyxl import Workbook
@@ -18,14 +19,14 @@ from openpyxl import Workbook
     #from openpyxl.reader.excel import load_workbook
     #from openpyxl.compat import range
     #from openpyxl.utils import get_column_letter
-
+import elevation
 # Local imports
 import col_functions
 import file_functions
 from file_functions import test_import
 
 #Functions
-def read_xlsx(wb_name, ws_name, min_col, min_row, max_col, max_row):
+def read_xlsx_orig(wb_name, ws_name, min_col, min_row, max_col, max_row):
     # Initialize openpyxl vars
     wb = load_workbook(str(wb_name))
     ws = wb[str(ws_name)]
@@ -86,20 +87,6 @@ def count_cols(workbook, worksheet):
     print("count cols:",col_count)
     return col_count
 
-def genColDict(workbook, worksheet):
-    # Dictionary associating output column to an input column number
-    # 'Output Column Name' : 'Input Column #'
-    wb = load_workbook(str(workbook))
-    ws = wb[str(worksheet)]
-    max_col = int(count_cols(workbook, worksheet))
-
-    print("In column dictionary...")
-    print("# of cols:\t",max_col)
-
-    # Save header in list
-    first_row = list(ws.rows)[0]
-    print(first_row)
-
 def merge_tables(observation_array, collector_array, input_wb, input_ws, num_rows=None):
     # Load XLSX file
     wb = Workbook()
@@ -107,7 +94,7 @@ def merge_tables(observation_array, collector_array, input_wb, input_ws, num_row
     ws.title = input_ws
 
     # Initialize values
-    month = ['i','ii','iii','iv','v','vi','vii','viii','ix','x','xi','xii']
+    month = ['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XIII']
     header_row = [  'iNaturalist ID',
                     'Collection Day 1',
                     'Month 1',
@@ -244,9 +231,14 @@ def merge_tables(observation_array, collector_array, input_wb, input_ws, num_row
     wb.save(filename = input_wb)
     print("Saved.")
 
-# Parse the command line arguments:
-    # Determine an input path, output path, and input file type
+#####################################
+
+
+
+
 def parse_cmd_line():
+    # Parse the command line arguments:
+    # Determine an input path, output path, and input file type
     print("Parsing command line arguments...")
 
     # Init vars
@@ -293,9 +285,13 @@ def parse_cmd_line():
         # We will use the split file path components
         out_file += "results/" + in_file.split('/')[1] + "/results.csv"
 
-    out_folder = "results/" + in_file.split('/')[1]
+    # Its necessary to append a '/' to the output folder so its treated as a dir
+    out_folder = "results/" + in_file.split('/')[1] + "/"
 
     # Create output folder
+        #print("creating output folder & files...\t",out_folder)
+        #print("out path:\t",os.path.dirname(out_folder))
+        #print("path exists?:\t",str(os.path.exists(os.path.dirname(out_folder))))
     if not os.path.exists(os.path.dirname(out_folder)):
         try:
             os.makedirs(os.path.dirname(out_folder))
@@ -309,17 +305,240 @@ def parse_cmd_line():
     # Return vars
     return in_file, out_file, in_type.lower()
 
+def read_csv_header(file_string):
+    file = open(file_string, "r") # Open CSV file
+    return file.readline() # Return only the first line of the CSV
+
+def read_xlsx_header(wb_name, ws_name):
+    wb = load_workbook(wb_name)
+    ws = wb[str(ws_name)]
+
+    max_col = list(string.ascii_lowercase)[ws.max_column - 1].upper()
+    print(max_col)
+    print(ws.max_column)
+
+    selection_str = "A1:" + max_col + "1"
+    return ws[selection_str].ecnode("utf8")
+
+# Two functions defined: One for reading CSV and one for Xlsx
 def read_csv(file_string):
-    print("Reading from CSV...")
+    print("\tReading from CSV...")
+
+    # Open CSV file
+    file = open(file_string, "r")
+
+    # Read the first line into header var
+    header_row = file.readline()
+
+    # Iterate through rest of file, saving in array
+    file_rows = []
+    for line in file:
+        file_rows.append(line)
+
+    # Returns the header row and line array
+    return header_row, file_rows
+
 def read_xlsx(file_string):
-    print("Reading from Excel spreadsheet...")
+    print("\tReading from Excel spreadsheet...")
 
 def read_data(file_string, file_type):
+    print("Reading data from input source...")
+    # Variables to capture the header row and following data
+    header = ''
+    file_rows = []
+
+    # Check which file type to read from
     if file_type == "csv":
+        header, file_rows = read_csv(file_string)
     elif file_type == "xlsx":
+        header, file_rows = read_xlsx(file_string)
     else:
         print("Invalid input file type. Exitting.")
         sys.exit()
+
+    # Strip header of extra characters and converts individuals chars into words
+    header = header.strip().split(',')
+    return header, file_rows
+
+def search_header(header, search_str):
+    # Locate the index of a string in the header row
+    print("\tLocating \'",search_str,"\'... ",end="")
+    for index, col in enumerate(header):
+        if col == search_str:
+            print("found at [",index,"].")
+            return index
+    print("no match found.")
+
+def print_out_header(line_to_print, csv_file):
+    with open(csv_file,'w') as file:
+        for index, col in enumerate(line_to_print):
+            # If the current col has a value, print it
+            if col != '':
+                file.write(str(col))
+            # If the current col isn't the last, print a comma
+            # This can be separated because both
+                #blank and full cols need a comma
+            if index < len(line_to_print):
+                file.write(',')
+        # Append newline as the last step to start the next row
+        file.write("\n")
+
+
+def print_out_row(line_to_print, csv_file):
+    with open(csv_file,'a') as file:
+        # Append generated row to output file
+        print(line_to_print)
+        for index, col in enumerate(line_to_print):
+            # If the current col has a value, print it
+            if col != '':
+                file.write(str(col))
+            # If the current col isn't the last, print a comma
+            # This can be separated because both
+                #blank and full cols need a comma
+            if index < len(line_to_print):
+                file.write(',')
+        # Append newline as the last step to start the next row
+        file.write("\n")
+
+def gen_output(out_header, out_file, in_header, in_data):
+    # Create rows of formatted data and append to output csv
+    print("Generating output data...")
+
+    # Print header row
+    print_out_header(out_header,out_file)
+
+    # Parse input rows
+    i = 0
+    for in_row in csv.reader(in_data, skipinitialspace=True):
+        i += 1
+        print("\n\t",i)
+
+        # Init the output row
+        out_row = []
+
+        # 0 Date Label Printed
+        # 1 Date Label Sent
+        # 2 Observation No.
+        # 3 Voucher No.
+        out_row.append(" ")
+        out_row.append(" ")
+        out_row.append(" ")
+        out_row.append(" ")
+
+        # 4 iNaturalist ID
+        id = in_row[search_header(in_header,"user_id")]
+        out_row.append(id)
+
+        # 5 Collector - First Name
+        # 6 Collector - First Initial
+        # 7 Collector - Last Name
+        u_name = in_row[search_header(in_header,"user_login")]
+        f_name, f_initial, l_name = col_functions.collector_name("data/usernames.csv",u_name)
+        out_row.append(f_name)
+        out_row.append(f_initial)
+        out_row.append(l_name)
+
+        # 8 Collection Day 1
+        # 9 Month 1
+        # 10 Year 1
+        # 11 Time 1
+        date1 = in_row[search_header(in_header,"observed_on")]
+        day1, month1, year1 = col_functions.date_1(date1)
+        time1 = col_functions.time_1(in_row[search_header(in_header,"time_observed_at")])
+        out_row.append(day1)
+        out_row.append(month1)
+        out_row.append(year1)
+        out_row.append(time1)
+
+        # 12 Collection Day 2
+        # 13 Moth 2
+        # 14 Year 2
+        # 15 Day 2 merge
+        # 16 Time 2
+        date2 = in_row[search_header(in_header,"field:trap removed")]
+        day2, month2, year2, merge2 = col_functions.date_2(date2)
+        time2 = col_functions.time_2(in_row[search_header(in_header,"field:trap removed")])
+        out_row.append(day2)
+        out_row.append(month2)
+        out_row.append(year2)
+        out_row.append(merge2)
+        out_row.append(time2)
+
+        # 17 Sample ID
+        sampleid = in_row[search_header(in_header,"field:sample id")]
+        out_row.append(sampleid)
+
+        # 18 Specimen ID
+        specimenid = in_row[search_header(in_header,"field:number of bees collected")]
+        out_row.append(specimenid)
+
+        # 19 Country
+        country = "USA"
+        out_row.append(country)
+
+        # 20 State
+        state = "OR"
+        if in_row[search_header(in_header,"place_state_name")] != "Oregon":
+            state = in_row[search_header(in_header,"place_state_name")]
+        out_row.append(state)
+
+        # 21 County
+        county = in_row[search_header(in_header,"place_county_name")]
+        out_row.append(county)
+
+        # 22 Location
+        # 23 Abbreviated Location
+        location = col_functions.location_guess(in_row[search_header(in_header,"place_guess")],"data/OR_cities.csv")
+        abbreviated_location = ''
+        out_row.append(location)
+        out_row.append(abbreviated_location)
+
+        # 24 Dec. Lat.
+        # 25 Dec. Long.
+        lat = col_functions.round_coord(in_row[search_header(in_header,"latitude")])
+        long = col_functions.round_coord(in_row[search_header(in_header,"longitude")])
+        out_row.append(lat)
+        out_row.append(long)
+
+        # 26 Pos Accuracy
+        pos_acc = in_row[search_header(in_header,"positional_accuracy")]
+        out_row.append(pos_acc)
+
+        # 27 Elevation
+        elevation = ""
+        out_row.append(elevation)
+
+        # 28 Collection method
+        collection_method = in_row[search_header(in_header,"field:oba collection method")]
+        out_row.append(collection_method)
+
+        # 29 Associated plant - family
+        # 30 Associated plant - species
+        # 31 Associated plant - iNaturalist url
+        family = in_row[search_header(in_header,"taxon_family_name")]
+        species = in_row[search_header(in_header,"scientific_name")]
+        url = in_row[search_header(in_header,"url")]
+        out_row.append(family)
+        out_row.append(species)
+        out_row.append(url)
+
+        # End of appending to output row
+
+        # Append generated row to output file
+        # If the row has multiple bees collected, expand by that many
+        if specimenid.isdigit() and int(specimenid) > 1:
+            print("multiple bees, print multiple times...",specimenid)
+            for i in range(1, int(specimenid)):
+                print("i:",i)
+                out_row[search_header(out_header,"Specimen ID")] = i
+                print_out_row(out_row,out_file)
+                #out_row[search_header(out_header,"Specimen ID")] = int(out_row[search_header(out_header,"Specimen ID")]) + 1
+        else:
+            print_out_row(out_row,out_file)
+
+        # Break for only 1 loop
+        #break
+
 
 def main():
     # Intro
@@ -337,30 +556,46 @@ def main():
     print("\tInput path:\t",input_file)
     print("\tOutput path:\t",output_file)
     print("\tInput type:\t",input_file_type)
+    print()
 
     # Choose which file reading function to call
-    print("Deciding which input function to use...")
-    read_data(input_file, input_file_type)
+    input_header, input_rows = read_data(input_file, input_file_type)
+    print()
 
-    # Init variables to pass into read_xlsx
-        #max_col = list(string.ascii_lowercase)[ int(count_cols(observation_wb,observation_ws[0])) - 1 ].upper()
-        #max_row = count_rows(observation_wb,observation_ws[0])
+    # Sort columns before writing output
+    output_header = "Date Label Printed,Date Label Sent,Observation No.,Voucher No.,iNaturalist ID,Collector - First Name,Collector - First Name Initial,Collector - Last Name,Collection Day 1,Month 1,Year 1,Time 1,Collection Day 2,Month 2,Year 2,Collection Day 2 Merge,Time 2,Sample ID,Specimen ID,Country,State,County,Location,Abbreviated Location,Dec. Lat.,Dec. Long.,Lat/Long Accuracy,Elevation,Collection method,Associated plant - family,Associated plant - species,Associated plant - Inaturalist URL".split(",")
+        # Revisit
+        #output_header2 = read_xlsx_header("data/4_16_19/Output_from_Script.xlsx","Sheet1")
+        #print(output_header2)
 
-    # Generate dictionary connecting column names to column number
-        #genColDict(observation_wb, observation_ws[0])
+    # Create output data
+    gen_output(output_header, output_file, input_header, input_rows)
+    print("writing to",output_file)
+    # Test elevation
+    ###################################
+    import requests
+    import pandas as pd
 
-    # Extract values from observations table
-        #observation_result = read_xlsx(observation_wb, observation_ws[0], min_col, min_row, max_col, max_row)
+    # script for returning elevation from lat, long, based on open elevation data
+    # which in turn is based on SRTM
+    def get_elevation(lat, long):
+        query = ('https://api.open-elevation.com/api/v1/lookup'f'?locations={lat},{long}')
+        r = requests.get(query).json()  # json object, various ways you can extract value
+        # one approach is to use pandas json functionality:
+        elevation = pd.io.json.json_normalize(r, 'results')['elevation'].values[0]
+        return elevation
+    #get_elevation(44.5993, -123.3157)
 
-    # Init variables to pass into read_xlsx
-        #max_col = list(string.ascii_lowercase)[ int(count_cols(observation_wb,observation_ws[1])) - 1 ].upper()
-        #max_row = count_rows(observation_wb,observation_ws[1])
+        #col_functions.elevation_from_coords(1,2)
 
-    # Extract values from collector ID table
-        #collector_result = read_xlsx(observation_wb, observation_ws[1], min_col, min_row, max_col, max_row)
+        #import geocoder
+        #g = geocoder.elevation([44.5993, -123.3157])
+        #print ("elevation meters:",g.meters)
 
-    # Generate Output Sheet
-        #merge_tables(observation_result, collector_result, output_wb, output_ws)
+    ###################################
+
+    print()
+
 
 if __name__ == '__main__':
     main()
